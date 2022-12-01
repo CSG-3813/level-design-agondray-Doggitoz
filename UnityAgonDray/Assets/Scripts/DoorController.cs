23ef7f46bd0686939ec92d4880d88ed5b931f45f
Doggitoz <cpwheeler4@gmail.com>
2022-11-30 21:44:34 -0600

    yera

--- UnityAgonDray/Assets/Scripts/DoorController.cs
+++ UnityAgonDray/Assets/Scripts/DoorController.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class DoorController : MonoBehaviour
 {
-
+    [SerializeField] AudioClip DoorOpenClip;
+    [SerializeField] AudioClip DoorCloseClip;
     GameObject door;
-
+    AudioSource AS;
     void Start()
     {
         door = transform.gameObject;
+        AS = GetComponent<AudioSource>();
     }
 
     public void OpenDoor()
     {
-        door.SetActive(false);
+        transform.position += Vector3.up * 10;
+        AS.clip = DoorOpenClip;
+        AS.PlayOneShot(DoorOpenClip);
     }
 
     public void CloseDoor()
