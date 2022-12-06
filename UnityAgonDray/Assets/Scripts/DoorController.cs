90861337511ce2fdc2219b82437c2a8f0e7b6923
Doggitoz <cpwheeler4@gmail.com>
2022-12-05 22:13:22 -0600

    animations

--- UnityAgonDray/Assets/Scripts/DoorController.cs
+++ UnityAgonDray/Assets/Scripts/DoorController.cs
@@ -9,15 +9,17 @@ public class DoorController : MonoBehaviour
     [SerializeField] AudioClip DoorCloseClip;
     GameObject door;
     AudioSource AS;
+    Animator anim;
     void Start()
     {
         door = transform.gameObject;
         AS = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
     }
 
     public void OpenDoor()
     {
-        transform.position += Vector3.up * 10;
+        anim.SetTrigger("OpenDoor");
         AS.clip = DoorOpenClip;
         AS.PlayOneShot(DoorOpenClip);
     }
