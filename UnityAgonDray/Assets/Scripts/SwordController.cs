16b1929792063bc4ee78a72a44921bf10b228b5c
Doggitoz <cpwheeler4@gmail.com>
2022-12-14 10:32:55 -0600

    new assets

--- UnityAgonDray/Assets/Scripts/SwordController.cs
+++ UnityAgonDray/Assets/Scripts/SwordController.cs
@@ -11,7 +11,7 @@ public class SwordController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        sword.GetComponent<Sword>().damage = damage;
+        
     }
 
     // Update is called once per frame
